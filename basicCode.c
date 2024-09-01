5b09f5a3ed84cbfb67591b7c31a628e0ba1c057b
James <jjaeyun24@gmail.com>
2024-09-01 17:25:04 +0800

    Add personalized greeting feature

--- basicCode.c
+++ basicCode.c
@@ -2,5 +2,8 @@
 int main(){
     printf("I forgot rest of programing language hello lmao");
     printf("\nSo chat gpt is the way to go");
+    char name[20];
+    scanf("%s",name);
+    printf("\nHello, [%s]!",name);
     
 }
